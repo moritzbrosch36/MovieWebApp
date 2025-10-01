93118a10ddcafa798d729fb92f5d385cfefec62a
moritzbrosch36 <moritz.brosch23@googlemail.com>
2025-10-01 12:17:29 +0200

    data_manager.py revised and missing function def get_movies(self, user_id) added.

--- data_manager.py
+++ data_manager.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 from models import db, User, Movie
 
-# Load .env file
+# .env Datei laden
 load_dotenv()
 
 class DataManager:
@@ -15,26 +15,29 @@ class DataManager:
         if not self.api_key or not self.omdb_url:
             raise ValueError("OMDB_API_KEY or OMDB_URL is missing in the .env file!")
 
-
     # --- User Management ---
-    def add_user(self, name):
+    def create_user(self, name):
+        """Add a new user to the database."""
         new_user = User(name=name)
         self.db_session.add(new_user)
         self.db_session.commit()
 
-
-    def get_all_users(self):
+    def get_users(self):
+        """Return a list of all users in the database."""
         return User.query.all()
 
-
     # --- Movie Management ---
+    def get_movies(self, user_id):
+        """Return all movies of a specific user."""
+        return Movie.query.filter_by(user_id=user_id).all()
+
     def add_movie(self, title, user_id):
-        """Adds a movie by fetching info from OMDb API. """
+        """Add a new movie to a user's favorites using OMDb API."""
         url = f"{self.omdb_url}?t={title}&apikey={self.api_key}"
         response = requests.get(url)
 
         if response.status_code != 200:
-            raise Exception("Error retrieving data from OMDb API.")
+            raise Exception("Error retrieving data from OMDb API")
 
         data = response.json()
 
@@ -51,22 +54,16 @@ class DataManager:
         self.db_session.add(new_movie)
         self.db_session.commit()
 
-
-    def get_all_movies(self):
-        return Movie.query.all()
-
+    def update_movie(self, movie_id, new_title):
+        """Update the title of a specific movie in the database."""
+        movie = Movie.query.get(movie_id)
+        if movie:
+            movie.title = new_title
+            self.db_session.commit()
 
     def delete_movie(self, movie_id):
+        """Delete a movie from the user's favorites."""
         movie = Movie.query.get(movie_id)
         if movie:
             self.db_session.delete(movie)
-            self.db_session.commit()
-
-
-    def update_movie(self, movie_id, title, genre, year):
-        movie = Movie.query.get(movie_id)
-        if movie:
-            movie.title = title
-            movie.genre = genre
-            movie.year = year
             self.db_session.commit()
